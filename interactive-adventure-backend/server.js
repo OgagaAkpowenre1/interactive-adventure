8fb563af0b054cbf0750c3b66fe7bbc0084ab9fc
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2024-12-13 08:40:36 +0000

    Changes to be committed:

--- interactive-adventure-backend/server.js
+++ interactive-adventure-backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config({path: '/workspace/interactive-adventure/security.env'});
+require('dotenv').config(); 
 
 
 const MONGO_URI = process.env.MONGO_URI
