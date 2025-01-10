cfa78f4959bb4c6e1d131ed27f4a43033a78bafa
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2025-01-10 06:01:47 +0000

    Changes to be committed:     created infobar and story details components

---
+++ interactive-adventure/src/components/InfoBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+
+// InfoBar contains rating, genre list, reading time
+
+const InfoBar = () => {
+    return(
+        <>
+        
+        </>
+    )
+}
--- interactive-adventure/src/pages/StoryList.jsx
+++ interactive-adventure/src/pages/StoryList.jsx
@@ -4,62 +4,15 @@ import styled from "styled-components";
 
 const Wrapper = styled(motion.div)``;
 
-const StoryListWrapper = styled(motion.ul)``;
 
-const Story = styled(motion.div)`
-    display: flex;
-
-`;
-
-const StoryImg = styled(motion.img)`
-    width: 400px;
-    height: 150px;
-`;
-
-const StoryInfo = styled(motion.div)``;
-
-const InfoItem = styled(motion.div)``;
-
-const InfoSection = styled(motion.div)``;
 
 const StoryList = () => {
   return (
     <>
+    
       <Wrapper>
         <StoryList>
-          <Story>
-            <StoryImg src="" alt="" />
-            <StoryInfo>
-              <h4>Title</h4>
-              <p>
-                Synopsis: Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit. Inventore suscipit harum accusantium quo quis quas, fugiat
-                itaque recusandae omnis pariatur, doloremque rerum animi quia
-                mollitia? Amet odit, consectetur et animi quia veritatis,
-                accusamus deleniti expedita, quis consequatur similique soluta
-                itaque!
-              </p>
-              <InfoSection>
-                <InfoItem>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                </InfoItem>
-                <InfoItem>
-                  <i className="fa-solid fa-clock"></i>
-                  <p>20 mins</p>
-                </InfoItem>
-                <InfoItem>
-                  <p>Genres</p>
-                  <ul>
-                    <li>Suspense</li>
-                    <li>Horror</li>
-                    <li>Thriller</li>
-                  </ul>
-                </InfoItem>
-              </InfoSection>
-            </StoryInfo>
-          </Story>
+          
         </StoryList>
       </Wrapper>
     </>
